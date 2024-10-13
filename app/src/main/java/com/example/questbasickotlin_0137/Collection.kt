66e1704a6c6bf10033293a580318807057dfce71
LingGrani <luthfiangger2510@gmail.com>
2024-10-13 07:46:51 +0700

    Create listof

---
+++ app/src/main/java/com/example/questbasickotlin_0137/Collection.kt
@@ -0,0 +1,14 @@
+package com.example.questbasickotlin_0137
+
+fun main() {
+    contohList()
+}
+
+fun contohList() {
+    println("=== List ===")
+
+    // List Read-only
+    val readOnlyAbjad = listOf("A", "B", "C")
+    println(readOnlyAbjad)
+
+}
