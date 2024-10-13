6968e0ef267e4956d2e0bbe56c82da7c1d2089e0
LingGrani <luthfiangger2510@gmail.com>
2024-10-13 07:59:37 +0700

    add if else statement

--- app/src/main/java/com/example/questbasickotlin_0137/ConditionalExpression.kt
+++ app/src/main/java/com/example/questbasickotlin_0137/ConditionalExpression.kt
@@ -2,4 +2,13 @@ package com.example.questbasickotlin_0137
 
 fun ConditionalStatement() {
     println("=== Conditional Expression ===")
+
+    //if-else
+    val number = 10
+    if (number > 0) {
+        println("Positive Number")
+    }
+    else {
+        println("Negative Number")
+    }
 }
