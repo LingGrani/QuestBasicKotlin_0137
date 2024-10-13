a5cd64408f9b1aad27bf0a2181829713d274c9c3
LingGrani <luthfiangger2510@gmail.com>
2024-10-13 08:14:40 +0700

    add uppercase Lambda Function

--- app/src/main/java/com/example/questbasickotlin_0137/LambdaFunction.kt
+++ app/src/main/java/com/example/questbasickotlin_0137/LambdaFunction.kt
@@ -1,2 +1,10 @@
 package com.example.questbasickotlin_0137
 
+fun uppercaseString(string: String): String {
+    return string.uppercase()
+}
+
+fun main() {
+    uppercaseString("Hello")
+    println({string: String -> string.uppercase()}("hello"))
+}
