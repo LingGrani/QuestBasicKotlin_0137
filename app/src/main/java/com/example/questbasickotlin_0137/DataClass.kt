bca392412f66b4c13a2a3ad6cc890e75cec75dad
LingGrani <luthfiangger2510@gmail.com>
2024-10-13 08:11:25 +0700

    add equal,copy function in data class

--- app/src/main/java/com/example/questbasickotlin_0137/DataClass.kt
+++ app/src/main/java/com/example/questbasickotlin_0137/DataClass.kt
@@ -4,3 +4,20 @@ data class Dataclass(
     val id: Int,
     var email: String
 )
+
+fun main() {
+    val data = Dataclass(1, "indra@gmail.com")
+    println(data)
+
+    //equal function
+    val data2 = Dataclass(1,"makan@gmail.com")
+    println(data == data2)
+
+    //copy function
+    val data3 = data.copy()
+    println(data3)
+
+    //copy with change
+    val data4 = data.copy(email = "sahar@gmail.com")
+    println(data4)
+}
